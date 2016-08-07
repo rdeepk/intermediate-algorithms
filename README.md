9f2f826f83c99322f298378172ae63cbe850a25f
Ramandeep Kaur <rdeepk@gmail.com>
2016-08-06 18:33:41 -0700

    DNA Pairing Algorithm

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # intermediate-algorithms
-Solutions to intermediate algorithms by &lt;a href = "https://www.freecodecamp.com">Free Code Camp&lt;/a>
+Solutions to intermediate algorithms by Free Code Camp
---
+++ dna-pairing.js
@@ -0,0 +1,24 @@
+/* 
+
+The DNA strand is missing the pairing element. Take each character, get its pair, and return the results as a 2d array.
+Base pairs are a pair of AT and CG. Match the missing element to the provided character.
+Return the provided character as the first element in each array.
+For example, for the input GCG, return [["G", "C"], ["C","G"],["G", "C"]]
+pairElement("ATCGA") should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]]
+
+*/
+
+
+function pairElement(str) {
+	var basePairs =  {T:"A", A:"T", G:"C", C:"G"};
+	matched = str.match(/[atcg]/gi);
+	var pairs = [];
+	matched.forEach(function(elem, ind){
+		pairs.push([elem, basePairs[elem]]);
+	});
+  return pairs;
+}
+
+pairElement("CTCTA");
+
+
